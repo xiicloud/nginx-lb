3c6ac361e717ba16cd2e283faee2764d362fade5
shellrock <hanwoody@gmail.com>
2016-01-29 20:38:35 +0800

    build from aws

--- build.sh
+++ build.sh
@@ -8,18 +8,16 @@ export IMAGE="nginx-lb"
 export TAGS="1.8 1.8.0"
 export BASE_IMAGE=microimages/nginx
 
-docker pull $REG_URL/$BASE_IMAGE
+docker pull $BASE_IMAGE
 
-docker tag -f $REG_URL/$BASE_IMAGE $BASE_IMAGE
-
-docker build -t $REG_URL/microimages/$IMAGE .
+docker build -t microimages/$IMAGE .
 
 #./test.sh
 
-echo "---> Starting push $REG_URL/microimages/$IMAGE:$VERSION"
+echo "---> Starting push microimages/$IMAGE:$VERSION"
 
 for t in $TAGS; do
-  docker tag -f $REG_URL/microimages/$IMAGE $REG_URL/microimages/$IMAGE:$t
+  docker tag -f microimages/$IMAGE microimages/$IMAGE:$t
 done
 
-docker push $REG_URL/microimages/$IMAGE
+docker push microimages/$IMAGE
