6eeaa060f80ea101741314da2998c1f1b211ae20
shellrock <hanwoody@gmail.com>
2016-02-03 15:32:30 +0800

    fix test

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -e
 docker rm -f "$CON_NAME" > /dev/null 2>&1 || true
 docker run -d --name $CON_NAME $IMAGE
 
-sleep 2
+sleep 5
 
 docker exec $CON_NAME ps ax|grep 'ngin[x]'
 docker exec $CON_NAME wget -O - http://localhost/
